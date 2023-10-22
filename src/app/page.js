906ea11701bb904c159d44faa817fe920f533fb4
David <adewaledavidola@gmail.com>
2023-10-22 04:11:39 +0100

    [cool parallax fx]

--- src/app/page.js
+++ src/app/page.js
@@ -1,92 +1,76 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
-import styles from './page.module.scss'
-import Image from 'next/image';
-import Lenis from '@studio-freight/lenis'
 import { useTransform, useScroll, motion } from 'framer-motion';
+import Image from 'next/image';
+import styles from './page.module.scss';
+import { useEffect, useRef } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { useDimension } from '@/hooks/useDimension';
 
 const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-]
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+  '10.jpg',
+  '11.jpg',
+  '12.jpg',
+];
 
 export default function Home() {
-  
-  const gallery = useRef(null);
-  const [dimension, setDimension] = useState({width:0, height:0});
-
+  const ref = useRef(null);
   const { scrollYProgress } = useScroll({
-    target: gallery,
-    offset: ['start end', 'end start']
-  })
-  const { height } = dimension;
-  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2])
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
+    target: ref,
+    offset: ['start end', 'end start'],
+  });
+  const { height } = useDimension();
 
-  useEffect( () => {
-    const lenis = new Lenis()
+  //   scrollYProgress.onChange((latest) => console.log(latest));
 
-    const raf = (time) => {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
 
-    const resize = () => {
-      setDimension({width: window.innerWidth, height: window.innerHeight})
-    }
-
-    window.addEventListener("resize", resize)
     requestAnimationFrame(raf);
-    resize();
-
-    return () => {
-      window.removeEventListener("resize", resize);
-    }
-  }, [])
+  }, []);
 
   return (
     <main className={styles.main}>
-      <div className={styles.spacer}></div>
-      <div ref={gallery} className={styles.gallery}>
-        <Column images={[images[0], images[1], images[2]]} y={y}/>
-        <Column images={[images[3], images[4], images[5]]} y={y2}/>
-        <Column images={[images[6], images[7], images[8]]} y={y3}/>
-        <Column images={[images[9], images[10], images[11]]} y={y4}/>
+      <div className={styles.spacer} />
+      <div ref={ref} className={styles.gallery}>
+        <Column images={[images.at(0), images.at(1), images.at(2)]} y={y} />
+        <Column images={[images.at(3), images.at(4), images.at(5)]} y={y2} />
+        <Column images={[images.at(6), images.at(7), images.at(8)]} y={y3} />
+        <Column images={[images.at(9), images.at(10), images.at(11)]} y={y4} />
       </div>
-      <div className={styles.spacer}></div>
+      <div className={styles.spacer} />
     </main>
-  )
+  );
 }
 
-const Column = ({images, y}) => {
+function Column({ images, y = 0 }) {
   return (
-    <motion.div 
-      className={styles.column}
-      style={{y}}
-      >
-      {
-        images.map( (src, i) => {
-          return <div key={i} className={styles.imageContainer}>
-            <Image 
-              src={`/images/${src}`}
-              alt='image'
-              fill
-            />
+    <motion.div className={styles.column} style={{ y }}>
+      {images.map((src, index) => {
+        return (
+          <div key={index} className={styles.imageContainer}>
+            <Image src={`/images/${src}`} fill alt="image" />
           </div>
-        })
-      }
+        );
+      })}
     </motion.div>
-  )
+  );
 }
